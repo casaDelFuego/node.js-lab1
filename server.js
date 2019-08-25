4bc99afdb0538fe748901ff9b54fb3fa3ac4651f
Olga <tselyuk.olga@gmail.com>
2019-08-25 14:38:03 +0200

    routes are working

--- server.js
+++ server.js
@@ -3,18 +3,57 @@ const server = express()
 
 server.use(express.static(`${__dirname}/static/`))
 
-server.get('/hello/:kek', (request, response) => {
-  // response.send('Hello from Express!')
-  response.send(request.params)
+server.get('/word', (request, response) => {
+  console.log('i hit the get word route')
+  const searchTerm = request.query.sw
+  const filtered = data.filter(w => w.searchWord === searchTerm)
+  // console.log(filtered)
+  response.send(filtered)
 })
-
 server.get('/word/', (request, response) => {
   response.send(data.map(w => w.searchWord))
 })
+server.post('/word', (request, response) => {
+  console.log('i hit the post word route')
+  const sw = request.query.sw
+  const lang = request.query.lang
+  const tran = request.query.tran
+  const tranLang = request.query.tranLang
+  data.push({ searchWord: sw, language: lang, translation: tran, translatedLanguage: tranLang })
+  console.table(data)
+  response.json(data).end()
+})
 
-// server.get('/word/', (request, response) => {
-//   response.send(data.map(w => w.searchWord))
-// })
+server.delete('/word', (request, response) => {
+  console.log('i hit the delete route')
+  const sw = request.query.sw
+  console.log(sw)
+  const needToDelete = data.find(w => w.searchWord === sw)
+  console.log(needToDelete)
+  const i = data.indexOf(needToDelete)
+  console.log(i)
+  data.splice(i, 1)
+  console.table(data)
+  response.json(data).end()
+})
+
+server.get('/words/', (request, response) => {
+  console.log('i hit word with two slash')
+  response.send(data.map(w => w.searchWord))
+})
+
+server.get('/lang/:x', (request, response) => {
+  const xLang = request.params.x
+  console.log(xLang)
+  const mappedData = data.map(l => {
+    if (xLang === 'English') {
+      return l.searchWord
+    } else {
+      return l.translation
+    }
+  })
+  response.json(mappedData).end()
+})
 
 const data = [
   { searchWord: 'pancake', language: 'English', translation: 'блин', translatedLanguage: 'Russian' },
@@ -28,6 +67,15 @@ const data = [
   { searchWord: 'biscuit', language: 'English', translation: 'печенье', translatedLanguage: 'Russian' },
   { searchWord: 'doughnut', language: 'English', translation: 'пончик', translatedLanguage: 'Russian' }
 ]
+
+server.use((request, response, next) => {
+  response.status(404).sendFile(`${__dirname}/static/404.html`)
+})
+
+server.use((error, request, response, next) => {
+  console.error(error.stack)
+  response.status(500).sendFile(`${__dirname}/static/500.html`)
+})
 
 const port = 3000
 
